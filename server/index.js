407f3a994cdd566c439a59bf012fff6a2205874f
haseebgardezi <55210037+haseebgardezi@users.noreply.github.com>
2020-07-26 00:01:36 +0500

    Add files via upload
    MongoDb Connection and routes setup

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require("./db");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//process.env.Port will check either 4000 port is empty or in used otherwise it will use another port;
+const Port = process.env.Port || 4000;
+
+const app = express();
+
+//latest version of react has built in bodyparser. you can remove bodyparser module from
+//your project if you want and only have to use.
+//app.use(express.json());
+
+app.use(bodyParser.json());
+//node will block the requests made by the other port numbers
+//except 4000
+//therefore we use cors to enable requests form other ports.
+app.use(cors({ origin: "http://localhost:3000" }));
+
+var addCourseRoutes = require("./routes/AddCourseRoutes");
+var addDWHoursRoutes = require("./routes/AddDWHoursRoutes");
+var timeTableRoutes = require("./routes/TimeTableRoutes");
+var addRoomsRoutes = require("./routes/AddRoomsRoutes");
+var userRoutes = require("./routes/UsersRoutes");
+const userAuthRoutes = require("./routes/UserAuthRoutes");
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", userAuthRoutes);
+app.use("/api", addCourseRoutes);
+app.use("/api", addDWHoursRoutes);
+app.use("/api", timeTableRoutes);
+app.use("/api", addRoomsRoutes);
+
+app.listen(Port, () => console.log("server listening at localhost:4000"));
