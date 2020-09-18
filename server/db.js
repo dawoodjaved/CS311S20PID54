263eaa97326902735edeef6b09f913e4ab707b2d
Dawoodjaveed <54910044+Dawoodjaveed@users.noreply.github.com>
2020-09-18 20:47:08 +0500

    online database linking

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-// const URL = "mongodb+srv://dawood_javeed:<password>@cluster0.fcq55.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const URL =
+  "mongodb+srv://dawood_javeed:dawood443@aoaproject.4rjiy.gcp.mongodb.net/ActivityScheduling?retryWrites=true&w=majority";
 
 //in order to remove both two deprication warnings we have to use more other two arguments
 //with connect  useNewUrlParser and useUnifiedTopology
 //also use useFindAndModify:true, useCreateIndex:true to remove depricating warnings
 mongoose.connect(
-  "mongodb://localhost:27017/ActivityScheduling",
+  URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
